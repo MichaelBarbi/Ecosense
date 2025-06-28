4c79af507e20edda32a1b8deebfd724904ed9411
michael <barbi.michael3@gmail.com>
2025-06-28 10:31:11 +0200

    added custom users model: Customer and Staff + registered them on admin

--- user/models.py
+++ user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Customer
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+# Staff
+class Staff(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
