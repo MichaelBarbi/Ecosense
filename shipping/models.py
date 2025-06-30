6260a91c8fa914fb544bc821403235f17d45b02d
michael <barbi.michael3@gmail.com>
2025-06-30 22:16:49 +0200

    Added ShippingAddress model

--- shipping/models.py
+++ shipping/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from user.models import Customer
 
-# Create your models here.
+# Customer shipping address
+class ShippingAddress(models.Model):
+
+    customer = models.OneToOneField(Customer, related_name="shippingAddress", on_delete=models.CASCADE, primary_key=True)
+    fullName = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    postalCode = models.CharField(max_length=20)
+    province = models.CharField(max_length=50)
