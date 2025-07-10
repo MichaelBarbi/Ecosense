a0cc2b1856ee76443a3bbf1110548ada9e2ab3de
michael <barbi.michael3@gmail.com>
2025-07-10 16:16:16 +0200

    Added common and crispy forms apps

--- ecosense/settings.py
+++ ecosense/settings.py
@@ -47,8 +47,15 @@ INSTALLED_APPS = [
     'order',
     'cart',
     'sensor',
-    'group'
+    'group',
+    'common',
+
+    'crispy_forms',
+    'crispy_bootstrap5'
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
