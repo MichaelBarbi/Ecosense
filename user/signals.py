2e916990ab3d3a3f65754aa163d9a8fb0e855f43
michael <barbi.michael3@gmail.com>
2025-07-11 16:07:51 +0200

    Delete back button in the profile page + Added init data for startapp

---
+++ user/signals.py
@@ -0,0 +1,33 @@
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+from .models import *
+from shipping.models import ShippingAddress
+
+@receiver(post_migrate)
+def create_initial_data(sender, **kwargs):
+
+    if not User.objects.all():
+
+        user = User.objects.create_user(
+            username='admin',
+            password='admin123!',  
+            email='324232@studenti.unimore.it',
+            first_name='mich',
+            last_name='barbi'
+        )
+
+        user.save()
+
+        customer = Customer.objects.create(
+            user=user
+        )
+
+        ShippingAddress.objects.create(
+            customer=customer,
+            fullName="Mich Barbi",
+            address='123 Main St',
+            city='Rome',
+            province='Rome',
+            postalCode='00100',
+            country='Italy'
+        )
