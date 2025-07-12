9cc8692106ad9a0dc878f974672382204101577a
michael <barbi.michael3@gmail.com>
2025-07-12 11:36:21 +0200

    Added Cart and Order models + cart context_processors + data test with signals

---
+++ order/context_processors.py
@@ -0,0 +1,21 @@
+from .models import Cart
+
+def cart_context(request):
+    cart = None
+    cart_items_count = 0
+
+    if request.user.is_authenticated:
+        try:
+            cart = Cart.objects.get(customer=request.user.customer)
+            cart_items = cart.cartItems.all()
+
+            for item in cart_items:
+                cart_items_count += item.quantity
+
+        except Cart.DoesNotExist:
+            pass
+
+    return {
+        'cart': cart,
+        'cart_items_count': cart_items_count
+    }
--- order/views.py
+++ order/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from user.views import customer_login_required
+from django.urls import reverse_lazy
+from django.views.generic.edit import DeleteView
+from .models import Cart
 
-# Create your views here.
+
+@method_decorator(customer_login_required, name='dispatch')
+class CartDeleteView(DeleteView):
+
+    model = Cart
+    success_url = reverse_lazy("catalog")
+
+    def get_object(self, queryset=None):
+        return self.request.user.customer.cart
